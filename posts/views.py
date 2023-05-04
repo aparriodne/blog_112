345f67651b842ce533384bcbd48874932703c6b5
Aaron <michaelproudfoot@ymail.com>
2023-05-04 09:47:40 -0700

    multiple additions made

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
 # Create your views here.
+
+class PostListView(ListView):
+    template_name = 'posts/list.html'
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = 'posts/detail.html'
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'posts/new.html'
+    model = Post
+    fields = ['title','subtitle','body','author']
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'posts/edit.html'
+    model = Post
+    fields = ['title','subtitle','body',]
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'posts/delete.html'
+    model = Post
+    success_url = reverse_lazy("post_list")
